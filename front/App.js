817a397743fba81ad3c890e9a7336c51fd379bf2
kelvinngure <knn5090@psu.edu>
2020-05-24 21:40:04 -0400

    Update App.js

--- front/App.js
+++ front/App.js
@@ -1,38 +1,68 @@
 import 'react-native-gesture-handler';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Platform, StatusBar, StyleSheet, View, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, View} from 'react-native';
 
 import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import AuthNavigator from "./navigation/AuthNavigator"
 import LoginScreen from "./screens/LoginScreen"
 import RegisterScreen from "./screens/RegisterScreen"
-import { set } from 'react-native-reanimated';
 import {LineProvider, LineConsumer} from "./LineContext"
+import BottomTabNavigator from './navigation/BottomTabNavigator';
 
 
 
 const Stack = createStackNavigator();
 
 //options={{headerShown: false}}
+const initialState = {
+  isAuthenticated: false,
+};
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGGED_IN":
+      return {
+        ...state,
+        isAuthenticated: true,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        isAuthenticated: false,
+      };
+    default:
+      return state;
+  }
+};
 
 export default function App(props) {
   const isLoadingComplete = useCachedResources();
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
   if (!isLoadingComplete) {
     return null; // create a splashscreen to return when loading
   } else {
-
     return (
-      <LineProvider value="offline">
+      <LineProvider value={{state, dispatch}}>
       <View style = {styles.container}>
         <NavigationContainer>
-        <LineConsumer>
-        { (line)=>{(line === "offline") ? <LoginScreen/>: console.log("online dude")}}
-        </LineConsumer>
+        
+          <LineConsumer>
+          {(state) => !state.isAuthenticated ? 
+          
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+              <Stack.Screen name= "Register" component={RegisterScreen}/>
+              <Stack.Screen name= "Login" component={LoginScreen}/>
+            </Stack.Navigator>
+          : 
+            <Stack.Navigator>
+              <Stack.Screen name= "Profile" component={BottomTabNavigator}/>
+            </Stack.Navigator>
+          
+          }
+          </LineConsumer>
+        
       </NavigationContainer>
     </View>
     </LineProvider>
