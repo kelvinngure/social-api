e896d609dab8d34b0f2593da4ac993a3f0385719
kelvinngure <knn5090@psu.edu>
2020-05-26 22:10:28 -0400

    logout

--- front/screens/FeedScreen.js
+++ front/screens/FeedScreen.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet, Dimensions } from 'react-native'
+import Colors from "../themes/Colors"
 
 export class FeedScreen extends Component {
     render() {
         return (
-            <View>
-            <Text>Hello</Text>
+            <View style={styles.profile}>
+
+                <View>
+                <Text>
+                    FEED SCREEN 
+                </Text>
+                </View>
             </View>
         )
     }
 }
 
 export default FeedScreen
+
+const styles = StyleSheet.create({
+    profile:{
+
+    },
+    pp:{
+        width: Dimensions.get('window').width,
+        height: 150,
+        alignItems:"center",
+        backgroundColor:Colors.blueTheme,
+        justifyContent: "center"
+    },
+    stretch: {
+      width: 80,
+      height: 80,
+      borderRadius: 40
+    }
+  });
