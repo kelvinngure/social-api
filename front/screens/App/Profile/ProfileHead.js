b3b9a6a80836fecacea99cce065b901395c1b139
Kelvin Ngure <62087345+kelvinngure@users.noreply.github.com>
2020-06-10 16:24:34 -0400

    Update ProfileHead.js

--- front/screens/App/Profile/ProfileHead.js
+++ front/screens/App/Profile/ProfileHead.js
@@ -1,12 +1,61 @@
 import React from 'react'
-import { Text, View} from 'react-native'
+import { Text, View, Image, StyleSheet} from 'react-native'
+import { LineConsumer } from "../../../contexts/LineContext"
 
 
 export default function ProfileHead (){
             return (
-                <View>
-                <Text>ProfileHead</Text>
-                </View>
+                <LineConsumer>
+                    {state => 
+                        (
+                        <View style = {styles.container}>
+                            <View style={styles.image}>
+                            <Image 
+                                style={styles.pp} 
+                                source={require('../../../assets/pp.jpg')}
+                            />
+                            </View>
+                            <View style={styles.info}>
+                            <Text style ={styles.name}>
+                                {state.state.user.fname} {state.state.user.lname}
+                            </Text>
+                            <Text style ={styles.notices}>
+                                345 notices
+                            </Text>
+                            </View>
+                        </View>
+                        )
+                          
+                    }
+                </LineConsumer>
             )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    info: {
+        alignItems: "center",
+        flex: 1,
+        paddingTop: 10
+    },
+    image: {
+        alignItems: "center",
+        height: "50%",
+        justifyContent: "center"
+    },
+    pp: {
+        width: 90,
+        height: 90,
+        borderRadius: 45
+        
+    },
+    name: {
+        fontSize: 25,
+    },
+    notices: {
+        fontSize: 17,
+    }
+  });
 
