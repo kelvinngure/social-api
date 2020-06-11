421b45d4e0c00982e7f1da70f891be3737ed364b
Kelvin Ngure <62087345+kelvinngure@users.noreply.github.com>
2020-06-11 16:16:49 -0400

    search functionality update
    search for individual user by name

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -34,8 +34,10 @@ router.route("/register")
             // hash the password 
             const salt = await bcrypt.genSalt()
             const hashedPWD = await bcrypt.hash(req.body.pwd, salt)
+            const uid = `${req.body.lname}_${req.body.fname}${Math.random().toString(36).substr(2, 13)}`
 
             const User = {
+                uid: uid,
                 email : req.body.email,
                 fname : req.body.fname,
                 lname : req.body.lname,
@@ -44,15 +46,17 @@ router.route("/register")
             }
 
             const payload = {
+                "uid": uid,
                 "email" : `${User.email}`,
-                "fname" : `${User.fname}`
+                "fname" : `${User.fname}`,
+                "lname" : `${User.lname}`
             }
 
             // user creation query
-            const creator = `INSERT INTO users(email, firstName, lastName, createDate, password) values ("${User.email}", "${User.fname}", "${User.lname}", "${User.ts}", "${User.pwd}")`
-            
+            const creator = `INSERT INTO users(idusers, email, name, createDate, password) values ("${User.uid}", "${User.email}", "${User.fname} ${User.lname}", "${User.ts}", "${User.pwd}")`
+            console.log(`${User.fname} ${User.lname}`)
             db.query(creator, (error, result) => {
-                            if (error) res.send("error while registering")
+                            if (error) res.send(error)
                             else{
                                 //jwt
                                 const token = jwt.sign(payload, key, ((err, token)=> {
@@ -64,7 +68,9 @@ router.route("/register")
                                     res.setHeader('Content-Type', 'application/json');
                                     res.send({"success":`welcome to boards ${User.fname}`, 
                                     "accessToken":`${token}`, 
-                                    "refreshToken": `${refreshToken}`})}
+                                    "refreshToken": `${refreshToken}`,
+                                    "user": payload
+                                })}
                                 }))
                                 console.log(`Account created. Welcome to Boards ${User.fname}`)
                                 
@@ -93,8 +99,10 @@ router.route("/login")
                 }
                 else{// when mysql returns data
                     const payload ={
+                        uid: result[0].idusers,
                         email: result[0].email,
-                        fname: result[0].firstName
+                        fname: result[0].firstName,
+                        lname: result[0].lastName
                     }
 
 
@@ -103,7 +111,10 @@ router.route("/login")
                         const checkPWD = await bcrypt.compare(req.body.pwd, result[0].password)
                         if(checkPWD){ // when the password is correct
                             const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, ((err, token)=> {
-                                res.status(200).send({"success":"welcome back to boards","token":`${token}`})}
+                                res.status(200).send({"success":"welcome back to boards",
+                                "token":`${token}`,
+                                "user": payload
+                            })}
                             )
                             )
                         }
@@ -121,6 +132,42 @@ router.route("/login")
             })
         } catch(e){(e) => {console.log(e)}}       
 })
+
+router.route("/searchUser")
+    .post(async (req, res) => {
+        try{
+            const trimmedQuery = req.body.name.trim()
+            const splitQuery = trimmedQuery.split(" ")
+            console.log(splitQuery.length)
+
+            let findUser;
+
+            if (splitQuery.length > 1){
+                findUser = `SELECT idusers, name FROM users WHERE name LIKE "%${splitQuery[0]}%" OR name LIKE "%${splitQuery[1]}%" `
+                
+            }else{
+                findUser = `SELECT idusers, name FROM users WHERE name LIKE "%${splitQuery[0]}%"`
+            }
+            
+
+            const userResults = await db.query(findUser, async (err, result) => {
+                if (err) {
+                    console.log(err)
+
+                }
+                else{// when mysql returns data
+                    if (result.length !== 0 ) {
+                        res.send(result)
+                    } 
+                    else{ // when the password is incorrect
+                        res.send([])
+                    }
+                }
+                
+            })
+        } catch(e){(e) => {console.log(e)}}       
+})
+
 
 
 
