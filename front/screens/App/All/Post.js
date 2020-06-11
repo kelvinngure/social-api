49405318ef731d567d6970e40658d8ed662c34b0
Kelvin Ngure <62087345+kelvinngure@users.noreply.github.com>
2020-06-11 11:14:30 -0400

    Create Post.js

---
+++ front/screens/App/All/Post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, ScrollView} from 'react-native'
+
+const Post = (props) => {
+        return (
+            <ScrollView>
+            <Text>POST</Text>
+            </ScrollView>
+        )
+    
+}
+
+export default Post;
+
