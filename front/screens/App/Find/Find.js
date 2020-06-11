223115c9e57421f15fb9fe3a99f73e8610ca975a
Kelvin Ngure <62087345+kelvinngure@users.noreply.github.com>
2020-06-11 16:34:52 -0400

    update search functionality

--- front/screens/App/Find/Find.js
+++ front/screens/App/Find/Find.js
@@ -1,16 +1,49 @@
 import React, { useState } from 'react'
-import { Text, ScrollView} from 'react-native'
+import { Text, ScrollView, View, StyleSheet, FlatList } from 'react-native'
 import { SearchBar } from 'react-native-elements';
+import axios from "axios";
+
 
 const Find = (props) => {
         const[search, setSearch] = useState("")
 
         const searcher = (query) => {
-            console.log(query)
+            console.log(searchResults)
         }
 
+        const searchResults = [
+            {
+                id:1, 
+                name: 'Sample',
+            }
+        ]
+        
+        const getUsers = (query) => {
+            url  =  `https://boards-server.herokuapp.com/users/searchUser`
+            body = {
+                name: query
+            }
+            axios.post(url, body, {
+                headers: { 'Content-Type': 'application/json' },
+            })
+            .then(
+                (res) => console.log(res)
+            )
+            .catch((e) => {
+                console.log("front error getting user")
+            }
+            )
+        }
+        function Result({ title }) {
+            return (
+                <View style={styles.item}>
+                <Text style={styles.title}>{title}</Text>
+              </View>
+            );
+          }
+
         return (
-            <ScrollView>
+            <View>
                 <SearchBar
                     placeholder="Type Here..."
                     onChangeText = {text => {
@@ -20,12 +53,30 @@ const Find = (props) => {
                     value= {search}
                     platform = "android"
                 />
+
+                <FlatList
+                    data={searchResults}
+                    renderItem={ ({ item }) => <Result title = {item.name} />}
+                    keyExtractor={item => item.id}
+                >
+                </FlatList>
+
+                </View>
                 
-                <Text>Find</Text>
-            </ScrollView>
         )
     
 }
 
 export default Find;
 
+const styles = StyleSheet.create({
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
+  });
