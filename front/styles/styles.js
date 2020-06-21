f621416c76ef470c6696b1567e02fc1002e1064f
kelvinngure <njuguna.kelvin22@gmail.com>
2020-06-21 16:09:14 -0400

    Create styles.js

---
+++ front/styles/styles.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { StyleSheet } from "react-native"
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: "red",
+        height: 30,
+        paddingHorizontal: 16
+    },
+    headerText: {
+        color: "white",
+        fontSize: 20
+    },
+    feed: {
+        paddingLeft: 16
+    }
+ })
